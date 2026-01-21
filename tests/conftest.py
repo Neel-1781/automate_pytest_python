a48c5c6bace7d53d123eaa2fd75f942f1b514984
Neel-1781 <neel.pa@browserstack.com>
2026-01-21 14:46:25 -0600

    Implement WebDriver fixture for BrowserStack tests
    Add a pytest fixture for BrowserStack WebDriver

---
+++ tests/conftest.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium import webdriver
+import os
+
+@pytest.fixture(scope="function")
+def driver():
+    # 1. Get credentials from Jenkins
+    username = os.environ.get('BROWSERSTACK_USERNAME')
+    access_key = os.environ.get('BROWSERSTACK_ACCESS_KEY')
+
+    # 2. Configure capabilities
+    caps = {
+        'os': 'Windows',
+        'os_version': '10',
+        'browser': 'Chrome',
+        'browser_version': 'latest',
+        'name': 'Jenkins Sample Test',
+        'build': 'Jenkins Build Python 3.6'
+    }
+
+    # 3. Connect to BrowserStack
+    bstack_url = 'https://' + username + ':' + access_key + '@hub-cloud.browserstack.com/wd/hub'
+    
+    driver = webdriver.Remote(
+        command_executor=bstack_url,
+        desired_capabilities=caps
+    )
+
+    # 4. Pass the driver to the test, then quit when done
+    yield driver
+    driver.quit()
